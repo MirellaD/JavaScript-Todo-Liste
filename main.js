9e006b293366666d2d24c4238817823ae8d99ee5
Mirella Damiano <128075133+Tragedyyy@users.noreply.github.com>
2024-06-27 19:10:54 +0200

    quick save

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function formHandler(event) {
+/*function formHandler(event) {
     event.preventDefault();
     if($("input[type=checkbox]").is(":checked")){
         let name = document.getElementById("name").value;
@@ -9,6 +9,11 @@ function formHandler(event) {
         let start = document.getElementById("from").value
         let end = document.getElementById("too").value
         
+        console.log(!name); //schaut ob der name ausgefüllt worden ist
+        if (!name){
+            alert("please name your Task")
+        }
+    
 
 
         addTaskFunk(name, color, notags, tagU, tagI, start, end);
@@ -17,10 +22,71 @@ function formHandler(event) {
     }else{
         alert("please select a Tag");
     }
+}*/
+
+function formHandler(event) {
+    event.preventDefault(); //reloaded die seite nicht beim submitten
+    let name = document.getElementById("name").value;
+    let color = document.querySelector('select[name="color"]').value
+    let notags = document.getElementById("none").checked;
+    let tagU = document.getElementById("urgend").checked;
+    let tagI = document.getElementById("important").checked;
+    let start = document.getElementById("from").value
+    let end = document.getElementById("too").value
+
+    if (!name){ //schaut ob der name ausgefüllt worden ist
+        alert("please name your Task")
+        return;
+    }
+    if (!color){ //schaut ob der name ausgefüllt worden ist
+        alert("please select a color")
+        return;
+    }
+    if (notags == false && tagU == false && tagI == false){ //schaut ob eines der checkboxen ausgefüllt worden ist
+        console.log("no checkmark");
+        alert("please choose at least one tag option");
+        return;
+    }
+
+    addTaskFunk(name, color, notags, tagU, tagI, start, end);
+        document.getElementById('taskForm').reset();
+        noNeew();
 }
+
 const form = document.getElementById("taskForm");
 form.addEventListener("submit", formHandler);
 
+
+function addTaskFunk(name, color, notags, tagU, tagI, start, end){
+    let container = document.getElementById("madeTasks");
+    let entry = document.createElement('div');
+    entry.className = "addedTask";
+
+    let checkedtag = "";
+    if(tagU == true){
+        checkedtag = "&#10710;";
+    }
+    if(tagI == true){
+        checkedtag = checkedtag + " &#8252;";
+    }
+    entry.innerHTML = `
+    <div class="wholeTask">
+        <div class="TaskBody">
+            <div class="TaskName">
+                <input type="checkbox" onclick="prozentding()" name="done" id="done">${name}
+                
+            </div>
+            <div class="taskTag">
+            <p>${checkedtag}</p>
+            </div>
+        </div>
+        <img src="trash.svg" id="delete" name="delete"  onclick="deleteT();" alt="Delete">
+        <p id="edit">&#9998</p>
+    </div>
+    `
+    container.appendChild(entry);
+    prozentding();
+}
 
 function neew(){
     document.getElementById("ontop").style.display = "block";
@@ -34,51 +100,26 @@ document.getElementById("ontop").style.display = "none";
 }
 
 
-function addTaskFunk(name, color, notags, tagU, tagI, start, end){
-    let container = document.getElementById("madeTasks");
-    let entry = document.createElement('div');
-    entry.className = "addedTask";
-
-    let checkedtag = "";
-    if(tagU == true){
-        checkedtag = "&#10710;";
-    }
-    if(tagI == true){
-        checkedtag = checkedtag + "&#8252;";
-    }
-    entry.innerHTML = `
-    <div class="wholeTask">
-        <div class="topTask">
-            <input type="checkbox" onclick="prozentding()" name="done" id="done">
-            <p>${name}</p>
-            <div class="taskTag">
-            <p>${checkedtag}</p>
-            </div>
-        </div>
-        <img src="trash.svg" id="delete" name="delete"  onclick="deleteT(this);" alt="Delete">
-        <p id="edit">&#9998</p>
-    </div>
-     `
-    container.appendChild(entry);
-    prozentding();
-}
 function prozentding(){
     const allecheck = document.querySelectorAll('#done');
     const totalcheck = allecheck.length;
     const donecheck = Array.from(allecheck).filter(checkbox => checkbox.checked).length;
     document.getElementById('erledigt').textContent= `Progress: ${donecheck} / ${totalcheck}`;
 }
-function deleteT(el){
+
+function deleteT(spec){
     let rlyDelete = confirm("do you really want to delete this task?")
     if (rlyDelete == true){
-        let element = el;
-        element.remove();
+        let entry = spec;
+        entry.remove();
     }
 }
+
 function nosing() {
     urgend.checked = false;
     important.checked = false;
     }
+    
 function yess() {
 none.checked = false;
 }
